f037f6431aba502891f2f1e431f0f3fe67ce757b
Kristina Hager <kristina.hager@gmail.com>
2015-09-14 20:44:17 -0500

    adding javadoc jar and sources jar

--- src/main/java/com/example/HelloPlanet.java
+++ src/main/java/com/example/HelloPlanet.java
@@ -1,13 +1,27 @@
 package com.example;
 
+/**
+ * HelloPlanet - a class modeling a planet
+ * in the most hello world type way
+ */
 public class HelloPlanet {
 
     private String name;
 
-    HelloPlanet(String name) {
+    /**
+     * Creates a HelloPlanet object
+     *
+     * @param name      the name of the new planet
+     */
+    public HelloPlanet(String name) {
         this.name = name;
     }
 
+    /**
+     * Returns the name for the HelloPlanet object
+     *
+     * @return      the name of the planet
+     */
     public String getName() {
         return name;
     }
